c402b7acbe1cd0723ff35f8949be51c3600c173f
unclewhd <316249593@qq.com>
2020-04-02 21:00:06 +0800

    练习

---
+++ 4_2/4_2.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//
+//int Test()
+//{
+//	int a = 10;
+//	int b = 20;
+//	return a, b;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int ret = Add(a,b)
+//		printf("%d",ret)
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	void printf_table(int n);
+//	printf_table(line);
+//	return 0;
+//}
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= n; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i*j);
+//		}printf("\n");
+//	}
+//}
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i<= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j<=i; j++)
+//		{
+//			printf("%-3d*%-3d=%-3d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	print_table(line);
+//	return 0;
+//}
